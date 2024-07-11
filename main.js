fee5393d59908a82ba937ca5005803330b1409d1
AbdiIbrahimAdan <abdishu98@gmail.com>
2024-07-11 16:47:43 +0300

    change some code and put some files together

--- main.js
+++ main.js
@@ -1,6 +1,6 @@
 
   import { initializeApp } from "https://www.gstatic.com/firebasejs/10.12.3/firebase-app.js";
-  import { getAuth, signInWithPopup, GoogleAuthProvider } from "https://www.gstatic.com/firebasejs/10.12.3/firebase-auth.js";
+  import { getAuth, signInWithPopup, GoogleAuthProvider, FacebookAuthProvider } from "https://www.gstatic.com/firebasejs/10.12.3/firebase-auth.js";
 
   const firebaseConfig = {
     apiKey: "AIzaSyBQdQnoNHg-A_2X2GUinC0XOLGaNqNZ_gk",
@@ -11,26 +11,50 @@
     appId: "1:64701005863:web:ca8954330c2914dfb9ce02"
   };
 
-
-  const app = initializeApp(firebaseConfig);
-
-  const auth = getAuth();
-  auth.languageCode = 'en';
-  const provider = new GoogleAuthProvider();
- 
-
-  const googleLogin = document.getElementById("google-login-btn");
-  googleLogin.addEventListener("click", function(){
   
-
-signInWithPopup(auth, provider)
-  .then((result) => {
-    const credential = GoogleAuthProvider.credentialFromResult(result);
-    const user = result.user;
-    window.location.href = "../logged.html",
-    console.log(user);
-  }).catch((error) => {
-    const errorCode = error.code;
-    const errorMessage = error.message; 
+  const app = initializeApp(firebaseConfig);
+  const auth = getAuth(app);
+  auth.languageCode = 'en';
+  
+  const provider = new GoogleAuthProvider();
+  
+  
+  function updateUserProfile(user) {
+    const userName = user.displayName || "Anonymous";
+    const userEmail = user.email || "No Email";
+    const userProfilePic = user.photoURL || "default-profile-pic.jpg";
+  
+    document.getElementById("userName").textContent = userName;
+    document.getElementById("userEmail").textContent = userEmail;
+    document.getElementById("userProfilePic").src = userProfilePic;
+  }
+  
+  
+  const googleLogin = document.getElementById("google-login-btn");
+  googleLogin.addEventListener("click", () => {
+    signInWithPopup(auth, provider)
+      .then((result) => {
+        const user = result.user;
+        console.log("User signed in with Google:", user);
+        updateUserProfile(user); 
+        window.location.href = "../logged.html";
+      })
+      .catch((error) => {
+        const errorCode = error.code;
+        const errorMessage = error.message;
+        console.error("Error during Google sign-in:", errorCode, errorMessage);
+      });
   });
-  })
+  
+ 
+  import { onAuthStateChanged } from "https://www.gstatic.com/firebasejs/10.12.3/firebase-auth.js";
+  onAuthStateChanged(auth, (user) => {
+    if (user) {
+      updateUserProfile(user); 
+      const uid = user.uid;
+      console.log("User is logged in with UID:", uid);
+    } else {
+      alert('Please create an account, then login'); 
+    }
+  });
+  
